fe0534be519b2cf123d38166b5946e77204d0f3d
Thiago José da Silva <tjs.thiago@gmail.com>
2023-02-07 22:26:09 -0300

    initial commit

---
+++ src/main/java/com/simian/controller/SimianController.java
@@ -0,0 +1,15 @@
+package com.simian.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimianController {
+
+    @GetMapping("/simian")
+    public String isSimian() {
+        return "It is working";
+    }
+
+}
