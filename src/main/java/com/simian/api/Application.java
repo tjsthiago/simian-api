a8d0e4c69d3c3f03efdec8159a222a742c7e6780
Thiago José da Silva <tjs.thiago@gmail.com>
2023-02-13 17:20:57 -0300

    removing InMemoryAnalysisDataRepository

--- src/main/java/com/simian/api/Application.java
+++ src/main/java/com/simian/api/Application.java
@@ -1,11 +1,8 @@
 package com.simian.api;
 
 import com.simian.api.entities.AnalysisData;
-import com.simian.api.external.repository.AnalysisDataRepository;
-import com.simian.api.external.repository.in.memory.InMemoryAnalysisDataRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 import java.util.ArrayList;
@@ -20,10 +17,5 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-	/*@Bean
-	public AnalysisDataRepository getRepository(){
-		return new InMemoryAnalysisDataRepository(analyzes);
-	}*/
 
 }
