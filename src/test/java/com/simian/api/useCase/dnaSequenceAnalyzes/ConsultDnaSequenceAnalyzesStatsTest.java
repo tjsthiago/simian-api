d6e1c011badbffec9f42f870b19740307e163031
Thiago José da Silva <tjs.thiago@gmail.com>
2023-02-13 11:00:35 -0300

    fix ConsultDnaSequenceAnalyzesStatsTest

--- src/test/java/com/simian/api/useCase/dnaSequenceAnalyzes/ConsultDnaSequenceAnalyzesStatsTest.java
+++ src/test/java/com/simian/api/useCase/dnaSequenceAnalyzes/ConsultDnaSequenceAnalyzesStatsTest.java
@@ -2,7 +2,6 @@ package com.simian.api.useCase.dnaSequenceAnalyzes;
 
 import com.simian.api.Application;
 import com.simian.api.controllers.consultDnaSequenceAnalyzesStats.DnaSequenceAnalyzesStats;
-import com.simian.api.entities.AnalysisData;
 import com.simian.api.useCase.consultDnaSequenceAnalyzesStats.ConsultDnaSequenceAnalyzesStats;
 import com.simian.api.useCase.ports.repository.IRepository;
 import org.junit.Test;
@@ -14,9 +13,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -39,7 +35,8 @@ public class ConsultDnaSequenceAnalyzesStatsTest {
 
     @Test
     public void shouldReturnARatioEqualToOneForTheSameNumberOfMutantsAndHumans() {
-        Mockito.when(repository.findAll()).thenReturn(mockStoredAnalyzes(1, 1));
+        Mockito.when(repository.count()).thenReturn(2);
+        Mockito.when(repository.countByIsSimian(true)).thenReturn(1L);
 
         DnaSequenceAnalyzesStats response = useCase.getDnaSequenceAnalyzesStats();
 
@@ -48,26 +45,12 @@ public class ConsultDnaSequenceAnalyzesStatsTest {
 
     @Test
     public void shouldReturnARatioEqualToZeroDotFourForTheSameNumberOfFortyMutantsAndOneHundredHumans() {
-        Mockito.when(repository.findAll()).thenReturn(mockStoredAnalyzes(40, 100));
+        Mockito.when(repository.count()).thenReturn(140);
+        Mockito.when(repository.countByIsSimian(true)).thenReturn(40L);
 
         DnaSequenceAnalyzesStats response = useCase.getDnaSequenceAnalyzesStats();
 
         assertEquals(0.4, response.getRatio());
-    }
-
-    private List<AnalysisData> mockStoredAnalyzes(int quantityOfSimians, int quantityOfHumans) {
-        List<AnalysisData> simians = new ArrayList<>(quantityOfSimians);
-        List<AnalysisData> humans = new ArrayList<>(quantityOfHumans);
-
-        for (int i = 0; i < quantityOfSimians; i++) {
-            simians.add(new AnalysisData("hash-" + String.valueOf(i), true));
-        }
-
-        for (int i = 0; i < quantityOfHumans; i++) {
-            humans.add(new AnalysisData("hash-" + String.valueOf(i), false));
-        }
-
-        return Stream.concat(simians.stream(), humans.stream()).collect(Collectors.toList());
     }
 
 }
